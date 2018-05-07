fe1dcc282d60222092ca83d6a93dec9b6d93a706
hugoRIMLINGER <38126181+hugoRIMLINGER@users.noreply.github.com>
2018-05-07 14:00:31 +0200

    Add files via upload

---
+++ userSpace/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+
+def home(request):
+    return HttpResponse("bonjour tout le monde -> vous êtes sur la page userSpace")
