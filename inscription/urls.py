b31d61ac373201a8f43aa5e76ade648bc4adea0a
sosorbn <38975549+sosorbn@users.noreply.github.com>
2018-05-07 17:24:50 +0200

    Add files via upload

--- inscription/urls.py
+++ inscription/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
+    path('sinscrire',views.inscription),
+    path('postulant',views.postulant),
+    path('intervenant',views.intervenant)
+
 ]
--- inscription/views.py
+++ inscription/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
-def home(request):
-    return HttpResponse("bonjour tout le monde -> vous etes sur la page d'inscription")
+def inscription(request):
+    return render(request,'inscription/sinscrire.html')
+
+def postulant(request):
+    return render(request,'inscription/postulant.html')
+
+def intervenant(request):
+    return render(request,'inscription/intervenant.html')
